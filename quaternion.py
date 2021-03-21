f4e42ff4211afaa3b3994248fcde0ca0455f5843
0keonx <kenan.majewski@gmail.com>
2021-03-21 19:31:54 +0100

    small fixes in format

--- quaternion.py
+++ quaternion.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 import numpy as np
 import pandas as pd
-
-
-# In[2]:
-
 
 def qa(THETA, PSI, FI):
     def qa0(THETA, PSI, FI):
@@ -27,9 +19,6 @@ def qa(THETA, PSI, FI):
     return [qa0(THETA,PSI,FI), qa1(THETA,PSI,FI), qa2(THETA,PSI,FI), qa3(THETA,PSI,FI)]       
 
 
-# In[3]:
-
-
 def qb(wx, wy, wz, dt = 0.005):
     w = np.sqrt(wx**2 + wy**2 + wz**2)
     
@@ -45,10 +34,6 @@ def qb(wx, wy, wz, dt = 0.005):
     
     return [qb0, qb1, qb2, qb3]
 
-
-# In[4]:
-
-
 def qq(qa, qb):
     q0 = qa[0] * qb[0] - qa[1] * qb[1] - qa[2] * qb[2] - qa[3] * qb[3] 
     
@@ -61,16 +46,10 @@ def qq(qa, qb):
     return [q0, q1, q2, q3]
 
 
-# In[5]:
-
-
 def CGH(q):
-    return np.array([[2 * (q[0]**2 + q[1]**2) - 1,       2 * (q[1] * q[2] - q[0]*q[3]),    2 * (q[1] * q[3] + q[0]*q[2])],
-                     [2 * (q[1] * q[2] + q[0]*q[3]),   2 * (q[0]**2 + q[2]**2) - 1 ,       2 *(q[2] * q[3] - q[0]*q[1])],
+    return np.array([[2 * (q[0]**2 + q[1]**2) - 1,     2 * (q[1] * q[2] - q[0]*q[3]),    2 * (q[1] * q[3] + q[0]*q[2])],
+                     [2 * (q[1] * q[2] + q[0]*q[3]),   2 * (q[0]**2 + q[2]**2) - 1 ,     2 *(q[2] * q[3] - q[0]*q[1])],
                      [2 * (q[1] * q[3] - q[0]*q[2]),   2 *(q[2] * q[3] + q[0]*q[1]),     2 * (q[0]**2 + q[3]**2) - 1]]) 
-
-
-# In[6]:
 
 
 def TFP(CGH):
@@ -86,9 +65,6 @@ def TFP(CGH):
     return [THETA(CGH), PSI(CGH), FI(CGH)]
 
 
-# In[7]:
-
-
 def not_ort_err(q):
     return q[0] + q[1] + q[2] + q[3] - 1
 
@@ -96,18 +72,8 @@ def q_length(q):
     return np.sqrt(q[0]**2 + q[1]**2 + q[2]**2 + q[3]**2)
 
 
-# In[14]:
-
-
 def quaternion(q_a, P, Q, R):
     q_b = qb(P, Q, R) 
     q = qq(q_a, q_b)
     tfp = TFP(CGH(q))
-    return [q, tfp[0]*(180/np.pi), tfp[1]*180/np.pi, tfp[2]*180/np.pi, not_ort_err(q), q_length(q)]          
-
-
-# In[13]:
-
-
-quaternion([0,0,0,1],1,1,0)
-
+    return [q, tfp[0], tfp[1], tfp[2], not_ort_err(q), q_length(q)]          

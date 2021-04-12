3ddb70223d8bee22e77d6919268498ab8431c64e
0keonx <kenan.majewski@gmail.com>
2021-04-12 15:06:23 +0200

    added serial port connection

---
+++ port.py
@@ -0,0 +1,61 @@
+import serial
+import time
+from threading import Thread
+
+class SerialRead:
+    def __init__(self, serial_port = '', serial_baud = ''):
+        self.sp = serial_port
+        self.sb = serial_baud
+
+        self.thread = None
+
+        self.rec = False    # IS RECEIVING
+        self.run = True     # IS RUNNING
+
+        self.data = []
+
+        print('Trying to connect to: ' + str(serial_port) + ' at ' + str(serial_baud) + ' BAUD.')
+
+        try:
+            self.sc = serial.Serial(serial_port, serial_baud, timeout=4)
+            print('Connected to ' + str(serial_port) + ' at ' + str(serial_baud) + ' BAUD.')
+        except:
+            print("Failed to connect with " + str(serial_port) + ' at ' + str(serial_baud) + ' BAUD.')
+            exit()
+
+    def start_serial(self):
+        if self.thread == None:
+            self.thread = Thread(target=self.back_thread)
+            self.thread.start()
+            while self.rec != True:
+                time.sleep(0.1)
+
+
+    def back_thread(self):   
+            time.sleep(1.0)  
+            self.sc.reset_input_buffer()
+            while (self.run):
+                #self.sc.readinto(self.data)
+                self.rec = True
+                print(self.sc.readline())
+
+    def close(self):
+        self.run = False
+        self.thread.join()
+        self.sc.close()
+        print('DISCONNECTED')
+        
+
+
+
+def port_config():
+    serial_port = '/dev/ttyACM0'
+    #portName = 'COM6'
+    serial_baud = 115200
+
+    sr = SerialRead(serial_port,serial_baud)
+    sr.start_serial()
+
+
+port_config()
+    
